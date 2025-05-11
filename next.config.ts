be282ab56ecb66920b6c5db66a635d9d974d9e8d
jsxbn <59411874+jsxbn@users.noreply.github.com>
2025-05-11 20:43:13 +0900

    ignore lint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Vercel을 포함한 production 빌드에서 Lint 에러가 나도 빌드가 멈추지 않음
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
